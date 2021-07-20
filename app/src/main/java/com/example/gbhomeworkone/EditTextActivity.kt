3286ecce02c792d3f0ea9dfbb7a0c35c06cdf406
ant <theant@mail.ru>
2021-07-20 20:11:22 +0400

    выполнил второе и третье задание можно было проще, но хотелось красоту навести.

---
+++ app/src/main/java/com/example/gbhomeworkone/EditTextActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gbhomeworkone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditTextActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_text)
+    }
+}
